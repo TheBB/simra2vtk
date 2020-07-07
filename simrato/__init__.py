8c6c1629b11aea8e838403f6c0cbe528ded71c1b
Eivind Fonn <evfonn@gmail.com>
2020-07-07 14:30:38 +0200

    Add UTM conversion arguments

--- simrato/__init__.py
+++ simrato/__init__.py
@@ -1,16 +1,30 @@
-import click
-from scipy.io import FortranFile
 from os.path import exists
 import sys
+
+import click
+from scipy.io import FortranFile
+import utm
+
+
+def utm_convert(x, y, src, tgt):
+    src_northern = src[-1].lower() == 'n'
+    tgt_northern = tgt[-1].lower() == 'n'
+    src_number = int(src[:-1])
+    tgt_number = int(tgt[:-1])
+    lat, lon = utm.to_latlon(x, y, src_number, northern=src_northern, strict=False)
+    x, y, *_ = utm.from_latlon(lat, lon, force_zone_number=tgt_number)
+    return x, y
 
 
 def reader_args(func):
     args = [
         click.option('--intwidth', default=4),
         click.option('--floatwidth', default=4),
+        click.option('--translate', type=(float, float), default=(0.0, 0.0)),
+        click.option('--utmconvert', nargs=2, type=str, default=None),
         click.option('--endian', type=click.Choice(['native', 'big', 'little']), default='native'),
         click.option('--res', 'resfile', default='cont.res'),
-        click.option('--mesh', 'meshfile', default='mesh.dat'),
+
     ]
     for arg in args:
         func = arg(func)
@@ -32,7 +46,8 @@ class Simra:
     }
 
     def __init__(self, meshfile='mesh.dat', resfile='cont.res', endian='native',
-                 floatwidth=4, intwidth=4, require_data=False):
+                 floatwidth=4, intwidth=4, translate=(0, 0), utmconvert=None,
+                 require_data=False):
         self.meshfile = meshfile
         self.resfile = resfile
 
@@ -48,6 +63,13 @@ class Simra:
             self.npts, self.nelems, self.imax, self.jmax, self.kmax, _ = f.read_ints(dtype=inttype)
             self.coords = f.read_reals(dtype=floattype).reshape(self.npts, 3).astype('f8')
             self.elems = f.read_ints(dtype=inttype).reshape(self.nelems, 8) - 1
+        self.coords[:,0] += translate[0]
+        self.coords[:,1] += translate[1]
+
+        if utmconvert:
+            x, y = utm_convert(self.coords[:,0], self.coords[:,1], *utmconvert)
+            self.coords[:,0] = x
+            self.coords[:,1] = y
 
         self.has_data = exists(resfile)
         if not self.has_data and require_data:
